444d39da392f72dd35d162c3dcdb4e95af8c9a0a
mfotiadis <mfotiadis@ots.gr>
2023-07-25 13:29:38 +0300

    Removed import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
