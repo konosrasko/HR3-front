f2f87e1edfdf5f4e6842916e599c0002bf631ef1
Konstantinos Raskopoulos <kraskopoulos@ots.gr>
2023-07-31 15:43:14 +0300

    prin thn ptwsh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { MyDetailsComponent } from './home/my-details/my-details.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent},
+  { path: 'home', component: HomeComponent,
+    children:[{ path: 'MyDetails', component: MyDetailsComponent}]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { HttpClientModule } from '@angular/common/http';
 import { MenuComponent } from './home/menu/menu.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { SidenavListComponent } from './home/sidenav-list/sidenav-list.component';
-
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {ReactiveFormsModule} from "@angular/forms";
-
 import {FormsModule} from "@angular/forms";
 import { EmployeeComponent } from './employee/employee.component';
 import {UserService} from "./services/user.service";
@@ -22,6 +17,8 @@ import { LoginModule } from './login/login.module';
 import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
 import { MyDetailsComponent } from './home/my-details/my-details.component';
+
+
 
 
 
