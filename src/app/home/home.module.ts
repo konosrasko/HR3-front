836521cae845c3b8da451c4684f25efed3949be7
mfotiadis <mfotiadis@ots.gr>
2023-08-02 11:03:22 +0300

    Added force login and 404 page.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,14 +5,12 @@ import { FooterModule } from './footer/footer.module';
 import { SidenavListModule } from './sidenav-list/sidenav-list.module';
 import { MenuModule } from './menu/menu.module';
 import { LandingComponent } from './landing/landing.component';
-import { AppRoutingModule } from '../app-routing.module';
 import { MyDetailsComponent } from './my-details/my-details.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { MatTableModule } from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
 import { LeavesComponent } from './leaves/leaves.component';
-// import { RequestsComponent } from './leaves/requests/requests.component';
-// import { AddComponent } from './leaves/add/add.component';
 import { AdminComponent} from "./admin/admin.component";
-import {MatTableModule} from "@angular/material/table";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatButtonModule} from "@angular/material/button";
 import { LeavesModule } from './leaves/leaves.module';
@@ -25,15 +23,15 @@ import {MatSortModule} from "@angular/material/sort";
     LandingComponent,
     MyDetailsComponent,
     LeavesComponent,
-    // RequestsComponent,
-    // AddComponent,
-    AdminComponent
+    AdminComponent,
   ],
   imports: [
     CommonModule,
     FooterModule,
     SidenavListModule,
     MenuModule,
+    AppRoutingModule,
+    MatTableModule,
     AppRoutingModule,
     LeavesModule,
     FormsModule,
