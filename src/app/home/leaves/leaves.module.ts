0e4f7bd48b87ecc018668f0e2f52b9f50104aa5b
sspirou <sspirou@ots.gr>
2023-08-02 13:05:08 +0300

    ADD REQUEST PAGE

--- src/app/home/leaves/leaves.module.ts
+++ src/app/home/leaves/leaves.module.ts
@@ -4,6 +4,13 @@ import { MatTableModule} from '@angular/material/table';
 import { AddComponent } from './add/add.component';
 import { RequestsComponent } from './requests/requests.component';
 import { MatSortModule } from '@angular/material/sort';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 
 
@@ -14,7 +21,14 @@ import { MatSortModule } from '@angular/material/sort';
   imports: [
     CommonModule,
     MatTableModule,
-    MatSortModule
+    MatSortModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule
   ]
 })
 export class LeavesModule { }
