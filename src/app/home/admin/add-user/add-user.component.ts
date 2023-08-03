f89952283776bbcaf07846e7452aa3e0dc261a2d
Stamatis Chatzis <schatzis@ots.gr>
2023-08-03 15:26:45 +0300

    added add-user v2

---
+++ src/app/home/admin/add-user/add-user.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import { Employee } from "../../../models/employee.model";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.scss']
+})
+
+export class AddUserComponent {
+
+  constructor(private route: Router) {
+  }
+
+  username = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+  employees = new FormControl('', [Validators.required]);
+  roles = new FormControl('', [Validators.required]);
+
+
+  Employees: Employee[] = [
+    {
+      "id" : 1,
+      "firstName" : "Simos",
+      "lastName" : "Spyroy",
+      "email" : "sspiroy@ots.gr",
+      "mobileNumber": 1234567891,
+      "address" : "Kyparissias 8"
+    },
+    {
+      "id" : 2,
+      "firstName" : "Mixail",
+      "lastName" : "Fotiadis",
+      "email" : "mfotiadis@ots.gr",
+      "mobileNumber": 1234567891,
+      "address" : "Sikelianoy 51"
+    }
+  ]
+
+  selectedEmployee = "";
+  selectedRole = "";
+  hide = true;
+
+  onSelectEmployee() {
+  }
+
+  onSelectRole(){
+
+  }
+
+  getErrorUsername() {
+    if (this.username.hasError('required')) {
+      return 'Πρέπει να εισάγεις username';
+    }else {
+      return "ok :)"
+    }
+  }
+
+  getErrorPass(){
+    if(this.password.hasError('required')){
+      return 'Πρέπει να εισάγεις password';
+    }else {
+      return "ok :)"
+    }
+  }
+
+  navigateTo(){
+    this.route.navigateByUrl('home/admin');
+  }
+}
