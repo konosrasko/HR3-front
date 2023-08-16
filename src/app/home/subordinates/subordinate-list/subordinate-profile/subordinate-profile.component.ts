fd4e96ab43a192d60c03b150f994e6d3fe315bcc
Theo <vasiliadisthodoris@gmail.com>
2023-08-16 15:14:20 +0300

    Subordinates-list/subordinate-profile

---
+++ src/app/home/subordinates/subordinate-list/subordinate-profile/subordinate-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subordinate-profile',
+  templateUrl: './subordinate-profile.component.html',
+  styleUrls: ['./subordinate-profile.component.scss']
+})
+export class SubordinateProfileComponent {
+
+}
