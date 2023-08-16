fd44c17c4773216afe5becec5a3c6fb1f26aff8b
mfotiadis <mfotiadis@ots.gr>
2023-08-16 13:41:14 +0300

    Completed the profile page.

--- src/app/home/my-details/my-details.component.ts
+++ src/app/home/my-details/my-details.component.ts
@@ -1,9 +1,10 @@
-import {ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
 import { UserService } from 'src/app/services/user.service';
 import { NgToastService } from "ng-angular-popup";
 import { DatePipe } from "@angular/common";
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { HttpStatusCode } from "@angular/common/http";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -14,44 +15,59 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 export class MyDetailsComponent implements OnInit {
     employee: Employee = new Employee();
     originalEmployee!: Employee;
+    myForm: FormGroup;
+    isLoading: boolean = true;
     // @ts-ignore
     token: string = localStorage.getItem('token');
 
 
     isEditMode: boolean = false;
-    isSaveMode: boolean = false;
+
     dataLoaded: boolean = false;
 
-    @ViewChild('editToggle')
-    editToggle!: ElementRef;
-
-    constructor(private userService: UserService, private cdr: ChangeDetectorRef, private tost:NgToastService, private date: DatePipe) {}
-
-    saveDetails() {
-      this.userService.saveEmployeeDetails(this.employee, this.token).subscribe({
-        next: data => {
-          this.isEditMode = false;
-          this.isSaveMode = true;
-          this.tost.success({detail: 'Επιτυχής Αποθήκευση!', summary: 'Η επεξεργασία των στοιχείων σας έγινε με επιτυχία!', position: "topRight", duration: 5000});
-        },
-        error: error => {
-          this.tost.error({
-            detail: 'Αποτυχία!',
-            summary: 'Υπήρξε πρόβλημα στην επικοινωνία με τον server!',
-            position: "topRight",
-            duration: 3000
-          });
-        }
-      });
+    constructor(private userService: UserService, private cdr: ChangeDetectorRef, private toast:NgToastService, private date: DatePipe) {
+      this.myForm = new FormGroup({
+        firstName: new FormControl({value: '', disabled: !this.isEditMode}, [Validators.required, Validators.maxLength(20)]),
+        lastName: new FormControl({value: '', disabled: !this.isEditMode}, [Validators.required, Validators.maxLength(20)]),
+        email: new FormControl({value: '', disabled: !this.isEditMode}, [Validators.required, Validators.maxLength(100), Validators.email]),
+        address: new FormControl({value: '', disabled: !this.isEditMode}, [Validators.required, Validators.maxLength(30)]),
+        mobileNumber: new FormControl({value: '', disabled: !this.isEditMode}, [Validators.required, Validators.maxLength(10)]),
+        hireDate: new FormControl({value: '', disabled: !this.isEditMode}, [Validators.required, Validators.maxLength(100)]),
+      })
 
     }
 
+  saveDetails() {
+    if (this.myForm.valid) {
+      this.employee = this.myForm.value;
+      this.employee.employeeId = this.originalEmployee.employeeId;
+      this.employee.hireDate = this.date.transform(this.employee.hireDate, 'yyyy-MM-dd');
+      this.userService.saveEmployeeDetails(this.employee, this.token).subscribe({
+        next: () => {
+          this.isEditMode = false;
+          this.toast.success({detail: 'Επιτυχής Αποθήκευση!', summary: 'Η επεξεργασία των στοιχείων σας έγινε με επιτυχία!', position: "topRight", duration: 5000});
+        },
+        error: error => {
+          if(error.status === HttpStatusCode.GatewayTimeout){
+            this.toast.error({
+              detail: 'Αποτυχία!',
+              summary: 'Υπήρξε πρόβλημα στην επικοινωνία με τον server!',
+              position: "topRight",
+              duration: 3000
+            });
+          }
+          console.log(error);
+        }
+      });
+    }
+  }
+
     cancelEdit() {
-      if (this.isEditMode) {
-        this.isEditMode = false;
-        this.cdr.detectChanges();
-        this.employee = {...this.originalEmployee}; // Revert changes here
-      }
+        if (this.isEditMode) {
+            this.isEditMode = false;
+            this.myForm.reset(this.originalEmployee);
+            this.myForm.disable();
+        }
     }
 
     loadEmployee(data: any){
@@ -59,6 +75,37 @@ export class MyDetailsComponent implements OnInit {
         this.originalEmployee = JSON.parse(data);
         this.employee.hireDate = this.date.transform(this.employee.hireDate, 'dd/MM/yyyy');
         this.originalEmployee.hireDate = this.date.transform(this.originalEmployee.hireDate, 'dd/MM/yyyy');
+    }
+
+    public myError = (controlName: string, errorName: string) =>{
+      return this.myForm.controls[controlName].hasError(errorName);
+    }
+
+    fetchDataFromBackend(): Promise<any> {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          const backendData = {
+            firstName: this.employee.firstName,
+            lastName: this.employee.lastName,
+            email: this.employee.email,
+            address: this.employee.address,
+            mobileNumber: this.employee.mobileNumber,
+            hireDate: this.employee.hireDate
+          };
+          resolve(backendData);
+        }, 1000); // Simulate delay for demonstration
+      });
+    }
+
+    toggleEditMode() {
+        this.isEditMode = !this.isEditMode;
+
+        if (this.isEditMode) {
+            this.myForm.enable(); // Enable all form controls
+        } else {
+            this.myForm.disable(); // Disable all form controls
+            this.myForm.reset(this.originalEmployee); // Reset form to original values
+        }
     }
 
     ngOnInit(): void {
@@ -72,5 +119,10 @@ export class MyDetailsComponent implements OnInit {
           );
           this.dataLoaded = true;
         }
+
+      this.fetchDataFromBackend().then(data => {
+        this.myForm.patchValue(data); // Assuming the response data keys match form control names
+        this.dataLoaded = true;
+      });
     }
 }
