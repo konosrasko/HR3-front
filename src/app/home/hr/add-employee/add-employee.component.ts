916b11a8958adaa3f244971ccadfbda1ab97bf6f
Konstantinos Raskopoulos <kraskopoulos@ots.gr>
2023-08-17 08:43:29 +0300

    errors

--- src/app/home/hr/add-employee/add-employee.component.ts
+++ src/app/home/hr/add-employee/add-employee.component.ts
@@ -3,9 +3,9 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { EmployeeService } from "../../../services/employee.service";
 import { Employee } from "../../../models/employee.model";
-import {DatePipe} from "@angular/common";
-import {Supervisors} from "../../../models/supervisors";
-import {Observable} from "rxjs";
+import { DatePipe } from "@angular/common";
+import { NgToastService } from "ng-angular-popup";
+
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
@@ -19,10 +19,7 @@ export class AddEmployeeComponent implements OnInit {
   token:String = localStorage.getItem('token')
   selectedOption?: Employee
 
-
-
-
-  constructor(private router: Router, private employeeService: EmployeeService,private datePipe:DatePipe ) {
+  constructor(private router: Router, private employeeService: EmployeeService,private datePipe:DatePipe,private toast: NgToastService  ) {
     this.EmployeeAddFormGroup = new FormGroup({
       firstName: new FormControl('', [Validators.required]),
       lastName: new FormControl('', [Validators.required]),
@@ -63,7 +60,7 @@ export class AddEmployeeComponent implements OnInit {
       this.employeeService.addEmployee(this.employee, this.token);
       this.router.navigateByUrl('/home/hr/all-employees');
       console.log(this.employee);
-    }else alert("ΛΑΘΟΣ")
+    }else this.toast.error({detail:"Μήνυμα λάθους",summary:"Ελέξτε τα πεδία !",position:"topRight",duration:5000})
   }
 
   cancel() {
