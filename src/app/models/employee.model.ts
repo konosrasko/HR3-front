81ce4f8e54dd7d19899abc087580fbc9dfedc556
Stamatis Chatzis <schatzis@ots.gr>
2023-08-24 13:48:02 +0300

    fixes

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -1,17 +1,18 @@
 export class Employee {
 
-     employeeId?: number
-     firstName?: string
-     lastName?: string
-     email?: string
-     mobileNumber?: string
-     address?: string
-     hireDate?: string | null
-     enabled?: boolean
-     supervisorId?: number
-     supervisorLastName?:string
+   employeeId?: number
+   firstName?: string
+   lastName?: string
+   email?: string
+   mobileNumber?: string
+   address?: string
+   hireDate?: string | null
+   enabled?: boolean
+   supervisorId?: number
+   supervisorLastName?:string
+   supervisorFirstName?:string
 
-    constructor( firstName: string, lastName:string, email:string, mobileNumber:string,address:string, hireDate:string , isEnabled:boolean,supervisorId:number, supervisorLastName: string) {
+    constructor( firstName: string, lastName:string, email:string, mobileNumber:string,address:string, hireDate:string , isEnabled:boolean,supervisorId:number, supervisorLastName: string, supervisorFirstName: string) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -21,5 +22,6 @@ export class Employee {
         this.enabled= isEnabled;
         this.supervisorId = supervisorId;
         this.supervisorLastName = supervisorLastName;
+        this.supervisorFirstName = supervisorFirstName;
      }
 }
