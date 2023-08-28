2063c8cc4d088c1ad336f7c49363d65fb1e64606
mfotiadis <mfotiadis@ots.gr>
2023-08-28 10:14:13 +0300

    Modal for leave request for approve and decline.

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../home/modal/modal.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+
+  constructor(private modal: MatDialog) { }
+
+  openModal(){
+    return this.modal.open(ModalComponent, {
+      width:"450px",
+      height:"190px",
+      disableClose: true
+    });
+  }
+}
